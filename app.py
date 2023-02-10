ab8d81b52be536880e4b21948414dc0faebbf537
ak-alam <alam.akbar1989@gmail.com>
2023-02-10 06:03:07 +0500

    testing

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def welcome():
     
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return "New Deployment"
+    return "New Tag Testing"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
