38d5d7b8d7e973ad9a901c8aaaadddd096c4dabb
ak-alam <alam.akbar1989@gmail.com>
2023-02-11 01:08:57 +0500

    testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
-    return "Health Check"
+    return "Health Check."
     
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
