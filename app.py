0bb73587b27b852075be5b6f9bd2ce38cfcd24ec
ak-alam <alam.akbar1989@gmail.com>
2023-02-10 23:55:09 +0500

    testing

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
-    return "Index Page Change"
+    return "Health Check"
     
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return "Everything New"
+    return "Hello API"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
