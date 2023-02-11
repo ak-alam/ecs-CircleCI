0ab5f0eda4e6752b750208ac9a06f54738ae8c00
ak-alam <alam.akbar1989@gmail.com>
2023-02-11 05:32:14 +0500

    Update app.py messgaes

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
-    return "Slack Integration Complete New"
+    return "print welcome"
     
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return "Hello API Slack Intergration New"
+    return "print hello world"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
