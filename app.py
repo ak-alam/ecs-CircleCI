ff1e9764e7df38008e8afb243fb4f3b63fa8a37c
ak-alam <alam.akbar1989@gmail.com>
2023-02-11 05:13:09 +0500

    testing

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
-    return "Slack Integration Complete"
+    return "Slack Integration Complete New"
     
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return "Hello API Slack Intergration"
+    return "Hello API Slack Intergration New"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
